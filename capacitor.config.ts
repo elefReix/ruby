8354c47b8e70ef82450762fa21f322dc75639893
jesus <jesus.elef@gmail.com>
2024-06-24 21:46:55 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ruby',
+  webDir: 'www'
+};
+
+export default config;
